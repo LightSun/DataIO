797ddc5b2031820be5a5c3b5ff40830f9bede103
lightsun <978136772@qq.com>
2019-04-16 15:01:52 +0800

    opt

--- DataIO/src/test/java/com/heaven7/java/data/io/temp/Person.java
+++ DataIO/src/test/java/com/heaven7/java/data/io/temp/Person.java
@@ -10,7 +10,7 @@ import java.util.Map;
 /**
  * @author heaven7
  */
-public class Person {
+public class Person<E> {
 
     public List<Number> list1;
     public List<? extends Number> list2;
@@ -23,6 +23,8 @@ public class Person {
     public List<Number>[] array1;
     public List<? super Number>[] array2;
 
+    public List<E> params; //类上的泛型只支持单泛型
+
     public static void main(String[] args) throws Exception{
 
         GenericNode list1 = parseField("list1");
@@ -33,24 +35,35 @@ public class Person {
         GenericNode map3 = parseField("map3");
         GenericNode array1 = parseField("array1");
         GenericNode array2 = parseField("array2");
+
+        Student person = new Student();
+        //
+        GenericNode node = parseField(person, "params");
     }
 
     private static GenericNode parseField(String fieldName) throws Exception{
         Field field = Person.class.getField(fieldName);
         Type type = field.getGenericType();
         GenericNode node = new GenericNode();
-        parseNodeImpl(type, node);
+        parseNodeImpl(null, type, node);
+        return node;
+    }
+    private static GenericNode parseField(Person<?> p, String fieldName) throws Exception{
+        Field field = p.getClass().getField(fieldName);
+        Type type = field.getGenericType();
+        GenericNode node = new GenericNode();
+        parseNodeImpl(p.getClass(), type, node);
         return node;
     }
 
-    private static void parseNodeImpl(Type type, GenericNode parent) {
+    private static void parseNodeImpl(Class clazz,Type type, GenericNode parent) {
         if(type instanceof ParameterizedType){
             Type[] types = ((ParameterizedType) type).getActualTypeArguments();
             List<GenericNode> subs = new ArrayList<>();
             GenericNode node;
             for (Type t : types){
                 node = new GenericNode();
-                parseNodeImpl(t, node);
+                parseNodeImpl(clazz, t, node);
                 subs.add(node);
             }
             parent.type = (Class<?>) ((ParameterizedType) type).getRawType();
@@ -59,37 +72,41 @@ public class Person {
             Type componentType = ((GenericArrayType) type).getGenericComponentType();
             parent.isArray = true;
             GenericNode node = new GenericNode();
-            parseNodeImpl(componentType, node);
+            parseNodeImpl(clazz, componentType, node);
             parent.addNode(node);
         }else if(type instanceof WildcardType){
             Type[] lowerBounds = ((WildcardType) type).getLowerBounds();
             Type[] upperBounds = ((WildcardType) type).getUpperBounds();
             if(Predicates.isEmpty(lowerBounds)){
-                parseNodeImpl(upperBounds[0], parent);
+                parseNodeImpl(clazz, upperBounds[0], parent);
             }else {
-                parseNodeImpl(lowerBounds[0], parent);
+                parseNodeImpl(clazz, lowerBounds[0], parent);
             }
         }else if(type instanceof TypeVariable){
-            Type[] types = ((TypeVariable) type).getBounds();
-            List<GenericNode> subs = new ArrayList<>();
-            GenericNode node;
-            for (Type t : types){
-                node = new GenericNode();
-                parseNodeImpl(t, node);
-                subs.add(node);
+            //indicates  Wildcard from object. that means only can be known as runtime.
+            //TypeVariable<?>[] types = ((TypeVariable) type).getGenericDeclaration().getTypeParameters();
+            ParameterizedType pt = (ParameterizedType) clazz.getGenericSuperclass();
+            if(pt == null){
+                throw new UnsupportedOperationException("must extend the generic super class");
             }
-            parent.subType = subs;
+            if(pt.getActualTypeArguments().length > 1){
+                //or else can't map them.
+                throw new UnsupportedOperationException("generic class. can only have one.");
+            }
+            parent.declareName = type.toString();
+            parseNodeImpl(clazz, pt.getActualTypeArguments()[0], parent);
         } else if(type instanceof Class){
-            parent.type = (Class<?>) type;
+            parent.type =(Class<?>) type;
         }else {
             throw new RuntimeException("" + type);
         }
     }
 }
 
-//List<Number> -> type = List.class, subType = GenricNode(Number)
+//List<Number> -> type = List.class, subType = GenericNode(Number)
 class GenericNode{
     Class<?> type;
+    String declareName;
     List<GenericNode> subType;
     boolean isArray;
 
@@ -100,3 +117,8 @@ class GenericNode{
         subType.add(node);
     }
 }
+//Person<String, Integer>
+class Student extends Person<String>{
+
+}
+
